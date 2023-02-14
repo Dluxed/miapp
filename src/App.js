f387ff5e6759dd8a029a66d0d7ca6679f156d3f3
Dluxed <118645102+Dluxed@users.noreply.github.com>
2023-02-13 19:43:17 -0700

    Agregado login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
+import { LoginButton } from './login';
+import { LogoutButton } from './logout';
+import { Profile } from './Profile';
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <h1 className='header'>Dogo Traductor
+        <nav>
+        {isAuthenticated ? (
+          <>
+            <Profile />
+            <LogoutButton />
+          </>
+        ) : (
+          <LoginButton />
+        )}
+        </nav>
+    </h1>
+    
+    
+    <div className='lenguages-container'>
+        <label for="translateFrom">Traducir del:</label>
+         <select name="" id="translateFrom">
+            <option value="es">Spanish</option>
+         </select>
+
+         <label for="translateTo"> al:</label>
+         <select name="" id="translateTo">
+         </select>
+    </div>
+
+   
+    <div class="result-container">
+        <div class="firstSection">
+            <textarea name="" id="inputTranslate" cols="30" rows="10" placeholder="Ingrese texto a traducir"></textarea>
+        </div>
+
+        <div class="secondSection">
+            <textarea name="" id="outputTranslate" cols="30" rows="10" placeholder="Resultado"></textarea>
+        </div>
+    </div>
+
+    
+    <button id="translate">Traducir</button>
+    <script src="./main.js"></script>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react";
+
+console.log(
+  process.env.REACT_APP_AUTH0_DOMAIN,
+  process.env.REACT_APP_AUTH0_CLIENT_ID
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider 
+    domain='dev-vvw5od2o7nmso5o2.us.auth0.com' 
+    clientId='S0jJ2rKdzAp2CwFbOI8Zj9rvswdFtWqL' 
+    redirectUri={window.location.origin}>
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
